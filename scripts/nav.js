75d409b71731a26031ba1b565b90b08af5e3f6f3
Suyog Subedi <subedisuyog555@gmail.com>
2021-01-01 14:21:45 +0545

    nefari-media index page mobile

---
+++ scripts/nav.js
@@ -0,0 +1,14 @@
+navbar = document.getElementById('navIcon')
+navItems = document.getElementById('navItems')
+
+// Navbar On Click 
+navbar.addEventListener("click", (e)=>{
+   if(navItems.style.display="hidden"){
+    navItems.style.display="block"
+    console.log("Not hiding")
+    
+   }else{
+    navItems.style.display="hidden"
+    console.log("Hello")
+   }
+})
